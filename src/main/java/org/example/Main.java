5b369d840a45a71d4645cc08f137ec6973cad95f
mihailponomarev <maikalpanda2009@yandex.ru>
2023-02-11 14:08:19 +0300

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,128 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!244");
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            String cardNum = "6228123123"; // номер карты
+            int pwd = 888888; // пароль
+            boolean flag = true; // объявляем логическую переменную
+            double surplus = 1000;// баланс
+
+            // интерфейс
+            System.out.println("--------- Добро пожаловать в банкомат ICBC ---------");
+
+        /** Ограничивает количество логинов **/
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("Пожалуйста, вставьте свою банковскую карту:");
+                String inputCard = input.next();
+                System.out.println("Пожалуйста, введите ваш пароль:");
+                int inputPwd = input.nextInt();
+
+                // проверяем аккаунт и пароль
+                if (inputCard.equals(cardNum) && inputPwd == pwd) {
+                    flag = true;
+                    break;
+                } else {
+                    if (i <= 2) {
+                        System.out.println("Извините, пароль неверный, у вас все еще есть" + (3 - i) + "Второй шанс!");
+                    } else {
+                        System.out.println("Извините, ваша карта заблокирована!");
+                        break;
+                    }
+                    flag = false;
+                }
+            }
+
+        /** Выберите функцию после успешного входа в систему */
+            if (flag) {
+                char answer = 'y';
+                while (answer == 'y') {
+                    System.out.println("Пожалуйста, выберите функцию: 1. Вывод средств 2. Пополнение счета 3. Проверка баланса 4. Перевод 5. Выход");
+                    int choice = input.nextInt();
+                    switch (choice) {
+                        case 1:
+                            // Выполнить операцию снятия
+                            System.out.println("---> Вывод средств");
+                            System.out.println("Пожалуйста, введите сумму вывода:");
+                            double getMoney = input.nextDouble();
+                            if (getMoney > 0) {
+                                if (getMoney <= surplus) {
+                                    if (getMoney % 100 == 0) {
+                                        System.out.println("Пожалуйста, возьмите свою банкноту! Баланс ￥" + (surplus - getMoney));
+                                    } else {
+                                        System.out.println("Извините, я не могу принять изменения!");
+                                    }
+                                } else {
+                                    System.out.println("Извините, баланс недостаточен!");
+                                }
+                            } else {
+                                System.out.println("Пожалуйста, введите правильную сумму:");
+                            }
+
+                            break;
+                        case 2:
+                            // выполнить депозитную операцию
+                            System.out.println("---> Депозит");
+                            System.out.println("Пожалуйста, расположите банкноты и поместите их в депозитный порт:");
+                            double saveMoney = input.nextDouble();
+                            if (saveMoney > 0 && saveMoney <= 10000) {
+                                if (saveMoney % 100 == 0) {
+                                    surplus += saveMoney;
+                                    System.out.println("Депозит успешен! Баланс ￥" + surplus);
+                                } else {
+
+                                    double backMoney = saveMoney % 100;
+                                    surplus = saveMoney + surplus - backMoney;
+                                    System.out.println("Депозит успешен! Баланс ￥" + surplus);
+                                    System.out.println("Пожалуйста, заберите сдачу ￥" + backMoney);
+                                }
+                            } else if (saveMoney > 10000) {
+                                System.out.println("Внесите депозит до 10000 юаней за раз, пожалуйста, внесите депозит партиями!");
+                            } else {
+                                System.out.println("Депонированная банкнота является поддельной банкнотой, и она недействительна и конфискована!");
+                            }
+                            break;
+                        case 3:
+                            // выполнить запрос баланса
+                            System.out.println("---> Проверить баланс");
+                            System.out.println("Баланс на вашей карте:" + surplus);
+                            break;
+                        case 4:
+                            // выполнить операцию переноса
+                            System.out.println("---> Трансфер");
+                            System.out.println("Пожалуйста, введите сумму перевода:");
+                            double goMoney = input.nextDouble(); // сумма перевода
+                            if (goMoney > 0) {
+                                if (goMoney <= surplus) {
+                                    System.out.println("Перевод успешно! Баланс is" + (surplus - goMoney));
+                                } else {
+                                    System.out.println("Извините, пожалуйста, убедитесь, что у вас достаточно средств на карте!");
+                                }
+
+                            } else {
+                                System.out.println("Перевод не выполнен! Пожалуйста, введите правильную сумму:");
+                            }
+                            break;
+                        case 5:
+                            // выполнить операцию выхода
+                            // System.out.println ("---> Exit");
+                            System.out.println("Спасибо за ваше использование!");
+                            return;
+                        default:
+                            System.out.println("Извините, выбранная вами функция неверна!");
+                            break;
+                    }// switch end
+                    System.out.println("Продолжить? Да / Нет");
+                    answer = input.next().charAt(0);
+
+                } // while end
+                System.out.println("Спасибо за ваше использование!");
+
+            }
+
+        }
     }
-}
